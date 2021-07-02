b2f5b2d9a541cc8468b3f56ab0efeb161c87169b
ruanhao <ruanha111@163.com>
2021-07-02 11:05:47 +0800

    add: model

--- code/data_augmentation.py
+++ code/data_augmentation.py
@@ -4,7 +4,14 @@ import cv2
 from PIL import Image
 import numpy as np
 
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
 import torch
+torch.manual_seed(0)
+torch.backends.cudnn.deterministic = False
+torch.backends.cudnn.benchmark = True
+import torch.nn as nn
 from torch.utils.data.dataset import Dataset
 import torchvision.transforms as transforms
 
@@ -37,8 +44,6 @@ train_path.sort()
 train_json = json.load(open('../input/mchar_train.json'))
 train_label = [train_json[x]['label'] for x in train_json]
 
-SVHNDataset(train_path, train_label).__getitem__(1)
-
 train_loader = torch.utils.data.DataLoader(
         SVHNDataset(train_path, train_label,
                    transforms.Compose([
@@ -53,5 +58,75 @@ train_loader = torch.utils.data.DataLoader(
     num_workers=10, # 读取的线程个数
 )
 
-for data in train_loader:
-    break
+
+# 定义模型
+class SVHN_Model1(nn.Module):
+    def __init__(self):
+        super(SVHN_Model1, self).__init__()
+        # CNN提取特征模块
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=(3, 3), stride=(2, 2)),
+            nn.ReLU(),  
+            nn.MaxPool2d(2),
+            nn.Conv2d(16, 32, kernel_size=(3, 3), stride=(2, 2)),
+            nn.ReLU(), 
+            nn.MaxPool2d(2),
+        )
+        # 
+        self.fc1 = nn.Linear(32*3*7, 11)
+        self.fc2 = nn.Linear(32*3*7, 11)
+        self.fc3 = nn.Linear(32*3*7, 11)
+        self.fc4 = nn.Linear(32*3*7, 11)
+        self.fc5 = nn.Linear(32*3*7, 11)
+        self.fc6 = nn.Linear(32*3*7, 11)
+    
+    def forward(self, img):        
+        feat = self.cnn(img)
+        feat = feat.view(feat.shape[0], -1)
+        c1 = self.fc1(feat)
+        c2 = self.fc2(feat)
+        c3 = self.fc3(feat)
+        c4 = self.fc4(feat)
+        c5 = self.fc5(feat)
+        c6 = self.fc6(feat)
+        return c1, c2, c3, c4, c5, c6
+    
+model = SVHN_Model1()
+
+
+# 损失函数
+criterion = nn.CrossEntropyLoss()
+# 优化器
+optimizer = torch.optim.Adam(model.parameters(), 0.005)
+
+loss_plot, c0_plot = [], []
+# 迭代10个Epoch
+for epoch in range(10):
+    for data in tqdm(train_loader):
+        c0, c1, c2, c3, c4, c5 = model(data[0])
+        loss = criterion(c0, data[1][:, 0]) + \
+                criterion(c1, data[1][:, 1]) + \
+                criterion(c2, data[1][:, 2]) + \
+                criterion(c3, data[1][:, 3]) + \
+                criterion(c4, data[1][:, 4]) + \
+                criterion(c5, data[1][:, 5])
+        loss /= 6
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        loss_plot.append(loss.item())
+        c0_plot.append((c0.argmax(1) == data[1][:, 0]).sum().item()*1.0 / c0.shape[0])
+        
+    print(epoch)
+
+
+plt.plot(loss_plot)
+plt.xlabel("Epoch")
+plt.ylabel("loss")
+plt.show()
+
+plt.plot(c0_plot)
+plt.xlabel("Epoch")
+plt.ylabel("First char Accuracy")
+plt.show()
